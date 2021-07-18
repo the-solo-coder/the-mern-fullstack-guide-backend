4449fe1b9e46faa70f2a11c643f04f6939c8e7ee
Aderson Oliveira <aolive24@my.centennialcollege.ca>
2021-07-18 08:51:14 -0400

    Create sample as of end of lesson 96

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -62,6 +62,29 @@ const createPlace = (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+
+  //create a copy of the previous place with the spread operator ...
+  const updatedPlace = {...DUMMY_PLACES.find((p) => p.id === placeId)};
+  //get the index of the place from the array to be used during update
+  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
+
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  console.log(DUMMY_PLACES);
+
+  res.status(200).json({ place: updatedPlace });
+};
+
+const deletePlace = (req, res, next) => {};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
