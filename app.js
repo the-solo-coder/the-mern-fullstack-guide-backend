d1027a2db51c9483a429f20b9b16657e7ff9568b
Aderson Oliveira <aolive24@my.centennialcollege.ca>
2021-07-18 08:11:30 -0400

    Create sample as of end of lesson 95

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 
 const placesRoutes = require("./routes/places-route");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/places", placesRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
