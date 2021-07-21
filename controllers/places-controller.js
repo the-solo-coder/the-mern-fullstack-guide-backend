84a700b30b9ab9938c77c557c03f27b22089fbd9
Aderson Oliveira <aolive24@my.centennialcollege.ca>
2021-07-21 12:38:35 -0400

    Create sample as of end of lesson 126

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -42,9 +42,17 @@ const getPlaceById = async (req, res, next) => {
   res.json({ place: place.toObject({ getters: true }) });
 };
 
-const getPlacesByUserId = (req, res, next) => {
+const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
-  const places = DUMMY_PLACES.filter((p) => p.creator === userId);
+  // const places = DUMMY_PLACES.filter((p) => p.creator === userId);
+
+  let places;
+  try {
+    places = await Place.find({ creator: userId }).exec();
+  } catch (err) {
+    const error = new HttpError("Couldn't retrieve places!", 500);
+    return next(error);
+  }
 
   if (!places || places.length === 0) {
     return next(
@@ -55,7 +63,9 @@ const getPlacesByUserId = (req, res, next) => {
     );
   }
 
-  res.json({ places });
+  res.json({
+    places: places.map((place) => place.toObject({ getters: true })),
+  });
 };
 
 const createPlace = async (req, res, next) => {
