ea3d29c9a134eda81f5730664e7db1773f709938
Aderson Oliveira <aolive24@my.centennialcollege.ca>
2021-07-26 17:12:05 -0400

    Create sample as of end of lesson 137

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -32,15 +32,17 @@ const getPlaceById = async (req, res, next) => {
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
 
-  let places;
+  //let places;
+  let userWithPlaces;
   try {
-    places = await Place.find({ creator: userId }).exec();
+    //places = await Place.find({ creator: userId }).exec();
+    userWithPlaces = await User.findById(userId).populate("places");
   } catch (err) {
-    const error = new HttpError("Couldn't retrieve places!", 500);
+    const error = new HttpError("Couldn't retrieve places!" + err, 500);
     return next(error);
   }
 
-  if (!places || places.length === 0) {
+  if (!userWithPlaces || userWithPlaces.places.length === 0) {
     return next(
       new HttpError(
         `Could not find a places with provided user id: ${userId}`,
@@ -50,7 +52,7 @@ const getPlacesByUserId = async (req, res, next) => {
   }
 
   res.json({
-    places: places.map((place) => place.toObject({ getters: true })),
+    places: userWithPlaces.places.map((place) => place.toObject({ getters: true })),
   });
 };
 
