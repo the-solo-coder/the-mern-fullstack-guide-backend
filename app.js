987983b96c224112755ea0e899addc07172db9ca
Aderson Oliveira <aolive24@my.centennialcollege.ca>
2021-07-27 15:56:18 -0400

    Create sample as of end of lesson 145

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ const app = express();
 
 app.use(express.json());
 
+//CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
@@ -29,7 +40,10 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect("mongodb+srv://mern-user:HRSNcgFfu2zK6NQ@cluster0.7qgoz.mongodb.net/mern?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
+  .connect(
+    "mongodb+srv://mern-user:HRSNcgFfu2zK6NQ@cluster0.7qgoz.mongodb.net/mern?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
   .then(() => {
     app.listen(5000);
   })
