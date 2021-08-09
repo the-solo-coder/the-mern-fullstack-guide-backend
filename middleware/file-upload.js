31678c274ffa14df8753274ebdc432fdf5a3d797
Aderson Oliveira <aolive24@my.centennialcollege.ca>
2021-08-09 17:19:30 -0400

    Create sample as of end of lesson 164

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -18,6 +18,11 @@ const fileUpload = multer({
       cb(null, uuidv4() + "." + ext);
     },
   }),
+  fileFilter: (req, file, cb) => {
+    const isValid = !!MIME_TYPE_MAP[file.mimetype];
+    let error = isValid ? null : new Error("Invalid file type");
+    cb(error, isValid);
+  }
 });
 
-module.export = fileUpload;
+module.exports = fileUpload;
