0fd785d6f5bb07b29079dd576931667dc403138d
Aderson Oliveira <aolive24@my.centennialcollege.ca>
2021-08-11 15:28:03 -0400

    Create sample as of end of lesson 167

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path");
 
 const express = require("express");
 const mongoose = require("mongoose");
@@ -10,6 +11,8 @@ const HttpError = require("./models/http-error");
 const app = express();
 
 app.use(express.json());
+
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
 
 //CORS
 app.use((req, res, next) => {
@@ -31,7 +34,7 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
-  if (req.file){
+  if (req.file) {
     fs.unlink(req.file.path, (err) => {
       console.log(err);
     });
